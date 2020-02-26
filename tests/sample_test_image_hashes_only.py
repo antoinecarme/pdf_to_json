0067a4752a621f47478bdbb4bda58221234f416c
Antoine Carme <antoine.carme@laposte.net>
2020-02-26 21:54:59 +0100

    Adde a new test

---
+++ tests/sample_test_image_hashes_only.py
@@ -0,0 +1,13 @@
+
+
+import pdf_to_json as p2j
+
+# web document : UDHR
+url = "https://www.ohchr.org/EN/UDHR/Documents/UDHR_Translations/eng.pdf"
+
+# Convert the document into a python dictionary
+lConverter = p2j.pdf_to_json.pdf_to_json_converter()
+lConverter.mImageHashOnly = True
+lDict = lConverter.convert(url)
+
+print(lDict)
